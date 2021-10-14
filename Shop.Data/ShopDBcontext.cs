70e8f027f38fc17d8955d7b7080fdcb6c458d3e1
Opilane TTHK <opilane@tthk.ee>
2021-10-14 15:37:02 +0300

    initial commit

---
+++ Shop.Data/ShopDBcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Core.Domain;
+using System;
+
+namespace Shop.Data
+{
+    public class ShopDbContext : IdentityDbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+            : base(options) { }
+        public DbSet<Product> Product { get; set; }
+    }
+}
