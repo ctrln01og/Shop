f59ca4aeb73b70a46e3f1f9f2f507799fc61cfaa
Ingvar Derešivski <inde999@gmail.com>
2021-10-28 11:43:33 +0300

    Init

---
+++ Shop.Data/ShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Core.Domain;
+
+namespace Shop.Data
+{
+    public class ShopDbContext : IdentityDbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Product { get; set; }
+        //kuidas yhendada aplikatsioon DB-ga
+    }
+}
