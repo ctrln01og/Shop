9218a2a4af5943c9a86621991bb18710f963b092
Ingvar Derešivski <inde999@gmail.com>
2021-10-28 15:47:59 +0300

    ProductContrAndView

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ShopDbContext _context;
+
+        public ProductController
+            (
+            ShopDbContext context
+            )
+        {
+            _context = context;
+        }
+
+        //ListItem
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var result = _context.Product
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new ProductListItem
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Value = x.Value,
+                    Weight = x.Weight
+                });
+
+            return View(result);
+        }
+    }
+}
