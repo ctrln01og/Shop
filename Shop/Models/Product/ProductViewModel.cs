6113d4755ae1a1fce9e083df30c40e3ad80695d1
Ingvar Derešivski <inde999@gmail.com>
2021-11-04 14:01:39 +0200

    EditViewChange

---
+++ Shop/Models/Product/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models.Product
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public double Weight { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+    }
+}
